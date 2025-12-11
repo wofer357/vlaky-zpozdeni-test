4f3681c903dbe5f343a283d26505a8ea765ca47e
Daniel Silha <wofer357@gmail.com>
2025-12-11 10:16:10 +0100

    Use 3 minute Dejvice offset for R and Sp trains

--- api/schedule.js
+++ api/schedule.js
@@ -146,7 +146,9 @@ export default async function handler(req, res) {
             const dest = item.trip.headsign;
             if (!processedTrains.has(trainNum) && !dest.includes('Masaryk') && !dest.includes('Hlavní')) {
                 const scheduledTime = new Date(item.arrival_timestamp.predicted);
-                const offsetMinutes = trainNum.startsWith('Sp') ? 3 : 5; // Sp vlaky posuneme o 2 min méně (oproti 5 -> 3)
+                const offsetMinutes = trainNum.startsWith('Sp') ? 3
+                    : trainNum.startsWith('R') ? 3
+                    : 5; // Sp/R vlaky posuneme o 2 min méně (oproti 5 -> 3)
                 const bridgeTime = new Date(scheduledTime.getTime() - (offsetMinutes * 60000));
 
                 bridgeSchedule.push({
